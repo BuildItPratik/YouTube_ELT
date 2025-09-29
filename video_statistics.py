6f10248937e2bfe488de043d2eeb36219070afb7
builditpratik <pratiklinkedin2005@gmail.com>
2025-09-29 14:30:25 +0530

    extracted video data with all properties in a list of dictionaries

--- video_statistics.py
+++ video_statistics.py
@@ -20,7 +20,7 @@ def get_playlist_id():
         #data['items'][0]['contentDetails']['relatedPlaylists']['uploads']
         channel_items = data['items'][0]
         channel_playlistId = channel_items['contentDetails']['relatedPlaylists']['uploads']
-        print(channel_playlistId)
+        # print(channel_playlistId)
         return channel_playlistId
     
     except requests.exceptions.RequestException as e:
@@ -55,7 +55,47 @@ def get_video_ids(playlistId):
     
     except requests.exceptions.RequestException as e:
         raise e
+    
+def extract_video_data(video_ids):
+
+    extracted_data = []
+
+    def batch_list(video_id_lst,batch_size):
+        for video_id in range(0,len(video_id_lst),batch_size):
+            yield video_id_lst[video_id: video_id + batch_size]
+
+    try:
+        for batch in batch_list(video_ids,max_results):
+            video_ids_str = ",".join(batch)
+
+            url = f'https://youtube.googleapis.com/youtube/v3/videos?part=contentDetails&part=snippet&part=statistics&id={video_ids_str}&key={API_KEY}'
+            response = requests.get(url)
+            response.raise_for_status()
+            data = response.json()
+
+            for item in data.get('items',[]):
+                video_id = item['id']
+                snippet = item['snippet']
+                contentDetails = item['contentDetails']
+                statistics = item['statistics']
+
+                video_data = {
+                    "video_id": video_id,
+                    "title": snippet['title'],
+                    "publishedAt": snippet['publishedAt'],
+                    "duration": contentDetails['duration'],
+                    "viewCount": statistics.get("viewCount",None),
+                    "likeCount": statistics.get("likeCount",None),
+                    "commentCount": statistics.get("commentCount",None),
+                }
+
+                extracted_data.append(video_data)
+
+        return extracted_data
+    except requests.exceptions.RequestException as e:
+        raise e
 
 if __name__ == "__main__":
     playlist_id = get_playlist_id()
     video_ids = get_video_ids(playlist_id)
+    video_data = extract_video_data(video_ids)
