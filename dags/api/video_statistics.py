dbb55504c415284d276651ba2b395503642a4936
builditpratik <pratiklinkedin2005@gmail.com>
2025-09-29 14:58:49 +0530

    converted functions to tasks and declared variables for API_KEY and CHANNEL_HANDLE

--- dags/api/video_statistics.py
+++ dags/api/video_statistics.py
@@ -2,14 +2,18 @@ import requests
 import json
 from datetime import date
 
-import os
-from dotenv import load_dotenv
-load_dotenv(dotenv_path="./.env")
+# import os
+# from dotenv import load_dotenv
+# load_dotenv(dotenv_path="./.env")
 
-API_KEY = os.getenv("API_KEY")
-CHANNEL_HANDLE = os.getenv("CHANNEL_HANDLE")
+from airflow.decorators import task
+from airflow.models import Variable
+
+API_KEY = Variable.get("API_KEY")
+CHANNEL_HANDLE = Variable.get("CHANNEL_HANDLE")
 max_results = 50
 
+@task
 def get_playlist_id():
     try:
         url =  f'https://youtube.googleapis.com/youtube/v3/channels?part=contentDetails&forHandle={CHANNEL_HANDLE}&key={API_KEY}'
@@ -26,7 +30,7 @@ def get_playlist_id():
     except requests.exceptions.RequestException as e:
         raise e
 
-
+@task
 def get_video_ids(playlistId):
     video_ids = []
     pageToken = None
@@ -56,6 +60,7 @@ def get_video_ids(playlistId):
     except requests.exceptions.RequestException as e:
         raise e
     
+@task
 def extract_video_data(video_ids):
 
     extracted_data = []
@@ -95,7 +100,7 @@ def extract_video_data(video_ids):
     except requests.exceptions.RequestException as e:
         raise e
     
-
+@task
 def save_to_json(extracted_data):
     file_path = f"./data/Youtube_data_{date.today()}.json"
 
