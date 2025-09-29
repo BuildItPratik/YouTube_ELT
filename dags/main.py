6b22c2eca099ba80888830de00f88a6dd1a4e869
builditpratik <pratiklinkedin2005@gmail.com>
2025-09-29 17:05:16 +0530

    created another dag to update the databases with data

--- dags/main.py
+++ dags/main.py
@@ -2,6 +2,8 @@ from airflow import DAG
 from pendulum import timezone
 from datetime import datetime,timedelta
 from api.video_statistics import get_playlist_id,get_video_ids,extract_video_data,save_to_json
+from datawarehouse.populating_tables import staging_table, core_table
+from airflow.operators.trigger_dagrun import TriggerDagRunOperator
 
 # Define the local timezone
 local_tz = timezone('Asia/Kolkata')
@@ -35,3 +37,19 @@ with DAG(
     save_to_json_task = save_to_json(extract_data)
 
     playlist_id >> video_ids >> extract_data >> save_to_json_task
+
+# DAG 2: update_db
+with DAG(
+    dag_id="update_db",
+    default_args=default_args,
+    description="DAG to process JSON file and insert data into both staging and core schemas",
+    catchup=False,
+    schedule=None,
+) as dag_update:
+
+    # Define tasks
+    update_staging = staging_table()
+    update_core = core_table()
+
+    # Define dependencies
+    update_staging >> update_core 
